30f2d8f0106ce3602ea44bcc0d48ef5d8fbc8fcd
Pelip Alexandr <sasha.pelip@gmail.com>
2023-11-13 04:54:25 +0300

    Code refactoring

---
+++ Assets/Scripts/Inspector/Editor/SphereEditor.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace Inspector
+{
+    [CustomEditor(typeof(Sphere))]
+    public class SphereEditor : Editor
+    {
+        private const string RadiusField = "_radius";
+        
+        public override void OnInspectorGUI()
+        {
+            Sphere sphere = (Sphere)target;
+
+            GUILayout.BeginHorizontal();
+            
+            UpdateSphereRadius(sphere);
+            DrawResetButton(sphere);
+            
+            GUILayout.EndHorizontal();
+        }
+
+        private void UpdateSphereRadius(Sphere sphere)
+        {
+            float radius = DrawRadiusSlider(sphere);
+            
+            GetRadiusField(sphere).SetValue(sphere, radius);
+
+            sphere.transform.localScale = Vector3.one * radius;
+        }
+
+        private float DrawRadiusSlider(Sphere sphere)
+        {
+            float radius = (float) GetRadiusField(sphere).GetValue(sphere);
+            radius = EditorGUILayout.Slider("Radius", radius, 0.01f, 5f);
+            return radius;
+        }
+
+        private void DrawResetButton(Sphere sphere)
+        {
+            if (GUILayout.Button("Reset"))
+                GetRadiusField(sphere).SetValue(sphere, 1f);
+        }
+
+        private FieldInfo GetRadiusField(Sphere sphere)
+        {
+            return sphere.GetType().GetField(RadiusField, BindingFlags.Instance | BindingFlags.NonPublic);
+        }
+    }
+}
--- Assets/Scripts/Inspector/Sphere.cs
+++ Assets/Scripts/Inspector/Sphere.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using UnityEngine;
+
+namespace Inspector
+{
+    public class Sphere : MonoBehaviour
+    {
+        [SerializeField] private float _radius = 1f;
+
+        private void Start()
+        {
+            transform.localScale = Vector3.one * _radius;
+        }
+    }
+}
